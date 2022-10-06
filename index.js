eaeb9bc326c350c69d68335f990a3829cf7d799a
the69nemo <thestigmata@yandex.ru>
2022-10-06 22:19:23 +0300

    add_btn-reset_&_some_fix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const tip = document.querySelector(".tip");
 const commentText = document.querySelector(".comment");
 const form = document.querySelector(".form");
 const button = document.querySelector(".button");
+const reset = document.querySelector(".reset");
 const audioWin = new Audio('./music/win.mp3');
 const audioLoss = new Audio('./music/loss.mp3');
 
@@ -22,13 +23,14 @@ function game() {
       commentText.textContent = '';
       countdownText.textContent = `Поздравляем, вы угадали число`;
       countdownText.style.color = 'red'
-      button.setAttribute('disabled', 'disabled');
-      button.style.backgroundColor = 'rgba(234, 240, 234, 0.582)';
+      reset.style.display  = 'block';
       audioWin.play();
+      form.style.display = 'none';
     } else if ((userNumber < randomNum | userNumber > randomNum) && count !== 0){
       switch (count) {
         case 6:
         case 5:
+          userNumberArr.push(userNumber);
           if (userNumber < randomNum) {
             tip.textContent = "Больше"
           } else {
@@ -36,31 +38,34 @@ function game() {
           }
           count--;
           countdownText.textContent = `У вас осталось ${count + 1} попыток`;
-          userNumberArr.push(userNumber);
           commentText.textContent = `Вы ввели: ${userNumberArr.join(', ')}`;
+          input.value = '';
           break;
         case 4:
         case 3:
         case 2:
+          userNumberArr.push(userNumber);
           if (userNumber < randomNum) {
             tip.textContent = "Больше"
           } else {
             tip.textContent = "Меньше"
           }
           count--;
-          userNumberArr.push(userNumber);
+
           countdownText.textContent = `У вас осталось ${count + 1} попытки`;
           commentText.textContent = `Вы ввели: ${userNumberArr.join(', ')}`;
+          input.value = '';
           break;
         case 1:
+          userNumberArr.push(userNumber);
           if (userNumber < randomNum) {
             tip.textContent = "Больше"
           } else {
             tip.textContent = "Меньше"
           }
-          userNumberArr.push(userNumber);
           countdownText.textContent = `У вас осталась последняя попытка`;
           commentText.textContent = `Вы ввели: ${userNumberArr.join(', ')}`;
+          input.value = '';
           count = 0
         default:
           break;
@@ -69,11 +74,16 @@ function game() {
       countdownText.textContent = `У вас не осталось попыток`;
       tip.textContent = '';
       commentText.textContent = '';
-      button.setAttribute('disabled', 'disabled');
-      button.style.backgroundColor = 'rgba(234, 240, 234, 0.582)'
+      form.style.display = 'none'
       audioLoss.play();
+      reset.style.display  = 'block';
     }
   });
+}
+
+function restart () {
+  console.log('click reset')
+  window.location.reload();
 }
 
 game();
